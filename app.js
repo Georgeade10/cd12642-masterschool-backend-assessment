96ae91a629514230118816940da8fe4c80a3a7be
Georgeade10 <georgeadeola10@gmail.com>
2023-02-27 22:09:02 +0100

    Part I - Setup

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the Unsplash API!' });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
