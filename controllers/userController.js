a64465dc827ade0d2fd5f4f3e7951bebddf5e1ed
Georgeade10 <georgeadeola10@gmail.com>
2023-03-18 14:37:04 +0100

    Routes and Middleware

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,98 @@
-//Import asyncHandler so that we can use it in our routes to trigger error handling middleware
+import asyncHandler from 'express-async-handler';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import User from '../models/userModel.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+};
+
+export const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please add all fields ');
+  }
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const newUser = new User({
+    name,
+    email,
+    password: hashedPassword,
+    token: generateToken(User.id),
+  });
+
+  await newUser.save();
+  res.status(201).json({ message: 'User created successfully', user: newUser });
+});
+
+export const loginUser = asyncHandler(async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ message: 'Incorrect email.' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Incorrect email or password.' });
+    }
+
+    const token = generateToken(user._id);
+
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: token,
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Invalid Credentials', error });
+  }
+});
+
+export const updateUser = asyncHandler(async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      req.body,
+      { new: true }
+    );
+    res
+      .status(200)
+      .json({ message: 'User updated successfully', user: updatedUser });
+  } catch (error) {
+    res.status(404).json({ message: 'Failed to update user', error });
+  }
+});
+
+export const deleteUser = asyncHandler(async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json({ message: 'User deleted successfully' });
+  } catch (error) {
+    res.status(404).json({ message: 'Failed to delete user', error });
+  }
+});
+
+export const getMe = asyncHandler(async (req, res) => {
+  res.status(200).json(req.user);
+});
+
+/* //Import asyncHandler so that we can use it in our routes to trigger error handling middleware
 import asyncHandler from 'express-async-handler';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
@@ -113,3 +207,4 @@ const generateToken = (id) => {
     expiresIn: '30d',
   });
 };
+ */
